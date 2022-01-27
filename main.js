db500fe2c921fcf03d747f010ca512cf0320f349
THE-WARRIOR-CR7 <82370377+THE-WARRIOR-CR7@users.noreply.github.com>
2022-01-26 17:52:34 -0800

    Add files via upload

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
-
-/*created by prashant shukla */
+rightWristX="";
+rightWristY="";
+gameStatus="";
 
 var paddle2 =10,paddle1=10;
 
@@ -23,13 +24,32 @@ var ball = {
 
 function setup(){
   var canvas =  createCanvas(700,600);
+  canvas.parent("canvas");
+  video=createCapture(VIDEO);
+	video.size(700,600);
+  video.hide();
+	poseNet = ml5.poseNet(video, modelLoaded);
+  poseNet.on('pose', gotPoses);
 }
+function modelLoaded() {
+	console.log('Model Loaded!');
+  }
 
+  function gotPoses(results)
+  {
+	if(results.length > 0)
+	{
+		console.log(results);
+	 rightWristX = results[0].pose.rightWrist.x;
+	  rightWristY = results[0].pose.rightWrist.y;
+	
+	}
+  }
 
 function draw(){
 
  background(0); 
-
+image(video,0,0,700,600);
  fill("black");
  stroke("black");
  rect(680,0,20,700);
